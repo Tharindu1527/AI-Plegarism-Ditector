1cd176837592881f9f55b87f5344d8ef013d7d78
Tharindu1527 <tharindubandara15270@gmail.com>
2024-10-20 01:29:33 +0530

    Add frontend code for LMS project

---
+++ lms_frontend/lms_frontend/src/components/Home.js
@@ -0,0 +1,84 @@
+function Home() {
+  return (
+    <div className="container mt-4">
+      
+      {/* Latest courses */}
+      <h3 className="pb-1 mb-4">Latest Courses</h3>
+      <div className="row">
+        <div className="col-md-3">
+          <div className="card">
+          <a href="#"><img src="logo512.png" className="card-img-top" alt="..." /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">Course title</a></h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+          <a href="#"><img src="logo512.png" className="card-img-top" alt="..." /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">Course title</a></h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+          <a href="#"><img src="logo512.png" className="card-img-top" alt="..." /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">Course title</a></h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+          <a href="#"><img src="logo512.png" className="card-img-top" alt="..." /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">Course title</a></h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* End Latest ourses */}
+
+      {/* Popular courses */}
+      <h3 className="pb-1 mb-4 mt-5">Populer Courses</h3>
+      <div className="row">
+        <div className="col-md-3">
+          <div className="card">
+          <a href="#"><img src="logo512.png" className="card-img-top" alt="..." /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">Course title</a></h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+          <a href="#"><img src="logo512.png" className="card-img-top" alt="..." /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">Course title</a></h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+          <a href="#"><img src="logo512.png" className="card-img-top" alt="..." /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">Course title</a></h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+          <a href="#"><img src="logo512.png" className="card-img-top" alt="..." /></a>
+            <div className="card-body">
+              <h5 className="card-title"><a href="#">Course title</a></h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* End Populer ourses */}
+    </div>
+  );
+}
+
+export default Home;
